a7a50a67185a15ddb04487fd2cf3cc83fb74dbd1
MAHTO ANJALI <159051588+MAHTO-ANJALI@users.noreply.github.com>
2024-05-05 11:48:56 +0530

    Update imposter.py

--- SHUKLAMUSIC/plugins/tools/imposter.py
+++ SHUKLAMUSIC/plugins/tools/imposter.py
@@ -30,8 +30,8 @@ ROY = [
     [
         InlineKeyboardButton(
             text="ᴀᴅᴅ ᴍᴇ",
-            url=f"https://t.me/Itz_SapnaMusicbot?startgroup=true"),
-        InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇ", url=f"https://t.me/SHIVANSH474")
+            url=f"https://t.me/PRISHUMUSICBOT?startgroup=true"),
+        InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇ", url=f"https://t.me/N3ON_BIO")
     ],
 ]
 
